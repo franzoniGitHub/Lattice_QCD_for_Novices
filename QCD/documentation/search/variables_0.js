47d792393cb21c609e9ec97eab4b7b1dae8a9449
stefanofranzoni10 <86156342+stefanofranzoni10@users.noreply.github.com>
2022-11-19 19:39:56 +0100

    Add fourth part of the documentation

---
+++ QCD/documentation/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['a',['a',['../SETTINGS__EXP_8h.html#a1031d0e0a97a340abfe0a6ab9e831045',1,'SETTINGS_EXP.h']]]
+];
---
+++ QCD/documentation/search/variables_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['epsilon',['epsilon',['../SETTINGS__EXP_8h.html#a4904cc82627458fdf6672ccc0b2802c7',1,'SETTINGS_EXP.h']]]
+];
